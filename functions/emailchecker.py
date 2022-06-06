e21a13bb125f178107d9d4edaa8aac2ee73791f8
Amrit Shenava <amritshenava98@users.noreply.github.com>
2022-06-06 17:06:05 +0000

    added functions for email, credit card and data sets

---
+++ functions/emailchecker.py
@@ -0,0 +1,15 @@
+import requests
+import os
+import random
+import json
+
+def isEmailFake(email):
+    temp_mail = json.loads(open('utils/email.json').read())
+    splitter = email.split("@")
+    domain = splitter[1]
+    for bogus_domain in temp_mail:
+      if(domain == bogus_domain):
+        return True
+      else:
+        return False
+  
